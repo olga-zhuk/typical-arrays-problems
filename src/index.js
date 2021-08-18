5cc289597267315896e3173aadc78e890282060c
Olga Zhuk <z-o-v2005@yandex.ru>
2021-08-18 23:14:23 +0300

    create file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (!array || array.length === 0) {
+        return 0;
+   }
+   else {
+      let minimum = array[0];
+        for (let i=1; i<array.length; i++){
+      if(array[i] < minimum){
+        minimum = array[i];   
+      }
+     }
+   return minimum;
+    }
+  }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    else {
+     let maximum = array[0];
+    for (let i=1; i<array.length; i++) {
+      if(array[i] > maximum) {
+        maximum = array[i];   
+      }
+     }
+   return maximum;}
+    }
+
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    else {
+    let sum = 0;
+      if (array.length == 0 ){
+            return 0;}
+            else { 
+                for (let i=0; i<array.length; i++)
+               { sum = sum + array[i] } 
+                return sum / array.length;
+            }           
+    }
 }
